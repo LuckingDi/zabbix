8b89c599a74bca97a96e5c8327f6c8f6cbf4ce2c
蓝色 <hahada@aliyun.com>
2020-07-15 19:25:42 +0800

    调整数据格式

--- src/test.py
+++ src/test.py
@@ -1,15 +1,69 @@
-a = (
-    (31121, 'net.if.in["enp4s0f0"]'),
-    (31122, 'net.if.in["enp4s0f1"]'),
-    (31133, 'net.if.out["enp4s0f0"]'),
-    (31041, 'system.cpu.load[all,avg15]'),
-    (31042, 'system.cpu.load[all,avg1]'),
-    (31043, 'system.cpu.load[all,avg5]'),
-    (31044, 'system.cpu.num'), (31073, 'system.cpu.util'),
-    (31099, 'vfs.dev.read.await[sda]'), (31100, 'vfs.dev.read.await[sdb]'),
-    (31105, 'vfs.dev.util[sda]'), (31106, 'vfs.dev.util[sdb]'),
-    (31107, 'vfs.dev.write.await[sda]'), (31108, 'vfs.dev.write.await[sdb]'),
-    (31070, 'vm.memory.size[pavailable]'), (31071, 'vm.memory.size[total]'))
+result = [
+	(
+		(29174, 10084, 'system.cpu.load[all,avg15]'),
+		(29170, 10084, 'system.cpu.load[all,avg1]'),
+		(29175, 10084, 'system.cpu.load[all,avg5]'),
+		(29161, 10084, 'system.cpu.num'),
+		(29200, 10084, 'system.cpu.util'),
+		(30980, 10084, 'vfs.dev.read.await[sda]'),
+		(30987, 10084, 'vfs.dev.util[sda]'),
+		(30981, 10084, 'vfs.dev.write.await[sda]'),
+		(29177, 10084, 'vm.memory.size[pavailable]'),
+		(29178, 10084, 'vm.memory.size[total]')
+	),(
+		(31041, 10325, 'system.cpu.load[all,avg15]'),
+		(31042, 10325, 'system.cpu.load[all,avg1]'),
+		(31043, 10325, 'system.cpu.load[all,avg5]'),
+		(31044, 10325, 'system.cpu.num'),
+		(31073, 10325, 'system.cpu.util'),
+		(31099, 10325, 'vfs.dev.read.await[sda]'),
+		(31100, 10325, 'vfs.dev.read.await[sdb]'),
+		(31105, 10325, 'vfs.dev.util[sda]'),
+		(31106, 10325, 'vfs.dev.util[sdb]'),
+		(31107, 10325, 'vfs.dev.write.await[sda]'),
+		(31108, 10325, 'vfs.dev.write.await[sdb]'),
+		(31070, 10325, 'vm.memory.size[pavailable]'),
+		(31071, 10325, 'vm.memory.size[total]')
+	), (
+		(31173, 10326, 'system.cpu.util'),
+		(31182, 10326, 'vm.memory.size[total]'),
+		(31184, 10326, 'vm.memory.util')
+	), (
+		(31303, 10327, 'system.cpu.util'),
+		(31312, 10327, 'vm.memory.size[total]'),
+		(31314, 10327, 'vm.memory.util')
+	), (
+		(31422, 10328, 'system.cpu.load[all,avg15]'),
+		(31423, 10328, 'system.cpu.load[all,avg1]'),
+		(31424, 10328, 'system.cpu.load[all,avg5]'),
+		(31425, 10328, 'system.cpu.num'),
+		(31454, 10328, 'system.cpu.util'),
+		(31479, 10328, 'vfs.dev.read.await[sda]'),
+		(31482, 10328, 'vfs.dev.util[sda]'),
+		(31483, 10328, 'vfs.dev.write.await[sda]'),
+		(31451, 10328, 'vm.memory.size[pavailable]'),
+		(31452, 10328, 'vm.memory.size[total]')
+	), (
+		(31539, 10329, 'system.cpu.load[all,avg15]'),
+		(31540, 10329, 'system.cpu.load[all,avg1]'),
+		(31541, 10329, 'system.cpu.load[all,avg5]'),
+		(31542, 10329, 'system.cpu.num'),
+		(31571, 10329, 'system.cpu.util'),
+		(31624, 10329, 'vfs.dev.read.await[sda]'),
+		(31627, 10329, 'vfs.dev.util[sda]'),
+		(31628, 10329, 'vfs.dev.write.await[sda]'),
+		(31568, 10329, 'vm.memory.size[pavailable]'),
+		(31569, 10329, 'vm.memory.size[total]')),]
+item_values = [(29174, 0.626006944444444), (29170, 0.6233125000000006), (29175, 0.6236666666666679), (29161, 32.0), (29200, 1.8407107423611133)]
+new_item = []
+for items in result:
+    item2 = [items[0][1]]
+    for item in items: # {'system.cpu.num': 32.0,'system.cpu.num': 32.0}
 
-for i in a:
-    print(i[0])
+        item1 = {}
+        for item_va in item_values:
+            if item[0] == item_va[0]:
+                item1[item[2]] = item_va[1]
+                item2.append(item1)    # [10329,{'system.cpu.num': 32.0,'system.cpu.num': 32.0}]
+    new_item.append(item2)
+print(new_item)
--- src/zabbix_mysql.py
+++ src/zabbix_mysql.py
@@ -1,11 +1,13 @@
 import pymysql
+import time
 
 db = pymysql.connect(host='123.196.116.236', user='root', passwd='Wode@0227', database='zabbix',
                      port=10001, charset='utf8')
 
 
-# 获取主机id [(10084, 'wode069', 0), (10325, 'wode007', 0), (10326, 'wode174', 0), ...}
+# 获取主机id [(10084, 'wode069', 0), (10325, 'wode007', 0), (10326, 'wode174', 0), ...]
 def get_host_id():
+    hosts = []
     try:
         # 使用 cursor() 方法创建一个游标对象 cursor
         cursor = db.cursor()
@@ -15,65 +17,137 @@ def get_host_id():
         cursor.execute(sql)
         # 获取所有记录列表
         results = cursor.fetchall()
-        hosts = []
         for i in results:
-            if i[2] == 0:
+            # 将停用的主机筛掉
+            if i[2] == 0 and i[1] != 'wode124' and i[1] != 'wode174':
                 hosts.append(i)
-        return hosts
     except Exception:
         print('查询主机id失败')
+    return hosts
 
 
 # 获取监控项id
 def get_item_id(hosts):
-    item_key0 = 'net.if.in["enp4s0f0"]'
-    item_key1 = 'net.if.out["enp4s0f0"]'
-    item_key2 = 'net.if.in["enp4s0f1"]'
-    item_key3 = 'net.if.out["enp4s0f0"]'
-    item_key4 = "system.cpu.load[all,avg1]"
-    item_key5 = "system.cpu.load[all,avg5]"
-    item_key6 = "system.cpu.load[all,avg15]"
-    item_key7 = "system.cpu.num"
-    item_key8 = "system.cpu.util"
-    item_key9 = "vfs.dev.read.await[sda]"
-    item_key10 = "vfs.dev.write.await[sda]"
-    item_key11 = "vfs.dev.util[sda]"
-    item_key12 = "vfs.dev.read.await[sdb]"
-    item_key13 = "vfs.dev.write.await[sdb]"
-    item_key14 = "vfs.dev.util[sdb]"
-    item_key15 = "vfs.dev.read.await[sdc]"
-    item_key16 = "vfs.dev.write.await[sdc]"
-    item_key17 = "vfs.dev.util[sdc]"
-    item_key18 = "vfs.dev.read.await[sdd]"
-    item_key19 = "vfs.dev.write.await[sdd]"
-    item_key20 = "vfs.dev.util[sdd]"
-    item_key21 = "vm.memory.size[total]"
-    item_key22 = "vm.memory.size[pavailable]"
-    #for i in hosts:
-    try:
-        cursor = db.cursor()
-        print(11)
-        sql2 = "select itemid,key_ from items where key_ in ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}'," \
-               "'{9}','{10}','{11}','{12}','{13}','{14}','{15}','{16}','{17}','{18}','{19}','{20}','{21}','{22}')" \
-               " and itemid in (select itemid  from items where hostid={23})"
+    item_key0 = "system.cpu.load[all,avg1]"
+    item_key1 = "system.cpu.load[all,avg5]"
+    item_key2 = "system.cpu.load[all,avg15]"
+    item_key3 = "system.cpu.num"
+    item_key4 = "system.cpu.util"
+    item_key5 = "vm.memory.size[total]"
+    item_key6 = "vm.memory.size[pavailable]"
+    item_key7 = "vfs.dev.read.await[sda]"
+    item_key8 = "vfs.dev.write.await[sda]"
+    item_key9 = "vfs.dev.util[sda]"
+    item_key10 = "vfs.dev.read.await[sdb]"
+    item_key11 = "vfs.dev.write.await[sdb]"
+    item_key12 = "vfs.dev.util[sdb]"
+    item_key13 = "vfs.dev.read.await[sdc]"
+    item_key14 = "vfs.dev.write.await[sdc]"
+    item_key15 = "vfs.dev.util[sdc]"
+    item_key16 = "vfs.dev.read.await[sdd]"
+    item_key17 = "vfs.dev.write.await[sdd]"
+    item_key18 = "vfs.dev.util[sdd]"
 
-        print(12)
-        sql = sql2.format(item_key0, item_key1, item_key2, item_key3, item_key4, item_key5, item_key6, item_key7,
-                          item_key8, item_key9, item_key10, item_key11, item_key12, item_key13, item_key14, item_key15,
-                          item_key16, item_key17, item_key18, item_key19, item_key20, item_key21, item_key22, hosts)
-        print(sql)
-        cursor.execute(sql)
-        count =cursor.fetchall()
-        return count
-    except Exception:
-        print("查询监控项id失败")
-    # 关闭数据库连接
+    all_item_id = []
+    for i in hosts:
+        try:
+            cursor = db.cursor()
+            sql2 = "select itemid,hostid,key_ from items where key_ in ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}','{13}','{14}','{15}','{16}','{17}','{18}')and itemid in (select itemid from items where hostid={19})"
+
+            sql = sql2.format(item_key0, item_key1, item_key2, item_key3, item_key4, item_key5, item_key6, item_key7,
+                              item_key8, item_key9, item_key10, item_key11, item_key12, item_key13, item_key14,
+                              item_key15, item_key16, item_key17, item_key18, i[0])
+            cursor.execute(sql)
+            count = cursor.fetchall()
+            all_item_id.append(count)
+        except Exception:
+            print("查询监控项id失败")
+
+    return all_item_id
+
+
+# 时间转换为时间戳
+def type_time(data_time):
+    # 转换为时间数组
+    time_array = time.strptime(data_time, "%Y-%m-%d %H:%M:%S")
+    # 转换为时间戳
+    timestamp = int(time.mktime(time_array))
+    return timestamp
+
+
+# 获取监控值
+def get_item_value(item_ids, time_from, time_till):
+    t_from = type_time(time_from)
+    t_till = type_time(time_till)
+    item_values = []
+    for item_id in item_ids:
+        try:
+            cursor = db.cursor()
+            sql2 = 'select itemid,clock,value from history where {0}>clock and clock>{1} and itemid={2} '
+            sql = sql2.format(t_till, t_from, item_id)
+            cursor.execute(sql)
+            results = cursor.fetchall()     # ((31303, 1594656703, 15.595506), (31303, 1594656763, 16.100756), ...)
+            if len(results) == 0:
+                sql3 = 'select itemid,clock,value from history_uint where {0}>clock and clock>{1} and itemid={2} '
+                sql0 = sql3.format(t_till, t_from, item_id)
+                cursor.execute(sql0)
+                results = cursor.fetchall()
+            sum_num = 0
+
+            for value in results:
+                sum_num += value[2]
+            avg_value = float(sum_num/len(results))
+            item_value = (item_id, avg_value,)
+            item_values.append(item_value)
+        except Exception:
+            print(item_id+"获取监控值错误")
     db.close()
+    return item_values  # [(31044, 32.0), (31073, 2.9307576826388937)]
 
 
 # 测试
 if '__main__' == __name__:
-    # hosts = get_host_id()
-    hosts = 10325
+    hosts = get_host_id()       # [(10084, 'wode069', 0), (10325, 'wode007', 0), (10326, 'wode174', 0), ...]
+    print('************获取hosts************')
+    print(hosts)
+    print('*********************************')
+    # hosts = [10329,10330]
     result = get_item_id(hosts)
+    print('************获取result************')
     print(result)
+    print('*********************************')
+    item_ids = []
+    for dicts in result:
+        for i in dicts:
+            item_ids.append(i[0])
+    print('**************获取监控项id*****************')
+    print(item_ids)
+    print('******************************************')
+    item_values = get_item_value(item_ids, '2020-07-14 00:00:00', '2020-07-15 00:00:00')    # [(31044, 32.0), (31073, 2.9307576826388937)]
+    print('**************获取监控项id以及值*****************')
+    print(item_values)  # [(29174, 0.626006944444444), (29170, 0.6233125000000006), (29175, 0.6236666666666679), (29161, 32.0), (29200, 1.8407107423611133)]
+    print('******************************************')
+    new_item = []       # [[10329,{'system.cpu.num': 32.0,'system.cpu.num': 32.0,...}],[10329,{'system.cpu.num': 32.0,'system.cpu.num': 32.0,...}]]
+    for items in result:
+        item2 = []
+        for item in items:
+            item2.append(item[1])
+            item1 = {}  # {'system.cpu.num': 32.0,'system.cpu.num': 32.0}
+            for item_va in item_values:
+                if item[0] == item_va[0]:
+                    item1[item[2]] = item_va[1]
+                    item2.append(item1)    # [10329,{'system.cpu.num': 32.0,'system.cpu.num': 32.0}]
+        new_item.append(item2)
+    print('**********平凑主机id，和监控项以及value**************')
+    print(new_item)
+    print('******************************************')
+    host_value = []
+    for item in new_item:
+        # for ii in item:     # [10329,{'system.cpu.num': 32.0,'system.cpu.num': 32.0}]
+        for host in hosts:      # [(10084, 'wode069', 0), (10325, 'wode007', 0), (10326, 'wode174', 0), ...]
+            if item[0] == host[0]:
+                item.append(host[1])
+                item.append(host[2])
+        host_value.append(item)
+
+    print(host_value)
